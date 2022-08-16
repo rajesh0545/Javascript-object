70e67f52b012c7c898b1f38e327b363ea6ba919d
rajesh <krajeshgowri7032@mail.com>
2022-08-16 17:21:22 +0530

    javascript-object

---
+++ object-literals.js
@@ -0,0 +1,5 @@
+emp = {
+    id202,name:"Abhi",salary:"25000"
+
+}
+console.log(emp.id+ ""+ emp.name+ ""+emp.salary);
---
+++ script.js
@@ -0,0 +1,6 @@
+
+var emp=new Object();  
+emp.id=101;  
+emp.name="Ravi Malik";  
+emp.salary=50000;  
+console.log(emp.id+" "+emp.name+" "+emp.salary);  
